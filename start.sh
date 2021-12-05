02098dc0682722dabd4d4598e74a6683a621cb27
Andrei Polyakov <ap120368@gmail.com>
2021-12-05 19:53:44 +0500

    SqlAlchemy added

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@ docker run --name fastapi -v ~/abp-m3u/populate-epg/wallet:/opt/oracle/instantcl
     -v ~/abp-m3u/fastapi:/app/app -v ~/abp-m3u/files:/app/files -p 8000:80 \
     -e MAX_WORKERS="2" \
     -e ATP_USER=$ATP_USER -e ATP_PASSWORD=$ATP_PASSWORD -e ATP_DSN=$ATP_DSN abpdock/fastapi_cx_oracle
+    -e TELEBOT=$TELEBOT
